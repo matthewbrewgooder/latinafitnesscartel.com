edd3d57e6339b6f37282b9f614c8758aea83f981
Isaac de Cuba <isaac.decuba@coolblue.nl>
2018-08-07 20:56:34 +0200

    update wordpress

--- wp-config.php
+++ wp-config.php
@@ -29,7 +29,7 @@ define('DB_USER', 'latinafi_root');
 define('DB_PASSWORD', '6WyZfrzQt.B.');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'lfc_mysql');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8mb4');
